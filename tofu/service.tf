71d362edb6a5b1c6a1f3e86d2d1938a111f3f8ce
Chase Lee <chase.lee@slalom.com>
2025-10-08 19:26:36 -0500

    Adding Tofu implementation of our deployment and service

---
+++ tofu/service.tf
@@ -0,0 +1,17 @@
+resource "kubernetes_service" "chase_service_demo" {
+  metadata {
+    name = "${var.resource_prefix}-chase-service"
+    namespace = kubernetes_namespace.pipelines4all.metadata[0].name
+  }
+  spec {
+    selector = {
+      app = kubernetes_deployment.chase_deploy_demo.metadata[0].labels.app
+    }
+    port {
+      port        = 80
+      target_port = 3000
+    }
+
+    type = "NodePort"
+  }
+}
